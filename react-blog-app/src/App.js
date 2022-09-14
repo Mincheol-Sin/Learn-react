ba5a5760747c34e5ab60c138fb71fca6d0cc27da
Mincheol Sin <cheole720@gmail.com>
2022-09-15 08:05:01 +0900

    Feat: useState와 props를 활용하여 상하위 컴포넌트 연결하는 법 학습

---
+++ react-blog-app/src/App.js
@@ -0,0 +1,63 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  let [글제목, 글제목변경] = useState([
+    "남자코트 추천",
+    "강남 우동맛집",
+    "파이썬 독학",
+  ]);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+
+      {글제목.map((a, i) => {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                setModal(true);
+                setTitle(i);
+              }}
+            >
+              {글제목[i]}{" "}
+              <span
+                onClick={() => {
+                  let copy = [...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy);
+                }}
+              >
+                👍
+              </span>
+              {따봉[i]}
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
+
+      {modal == true ? <Modal 글제목={글제목} title={title} /> : null}
+    </div>
+  );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal">
+      <h4>{props.글제목[props.title]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button>글수정</button>
+    </div>
+  );
+}
+
+export default App;
