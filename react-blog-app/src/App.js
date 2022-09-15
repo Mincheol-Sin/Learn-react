0d8ba6a5d4a0b095701a2c6acd1ffdbd238a11d1
Mincheol Sin <cheole720@gmail.com>
2022-09-15 21:49:03 +0900

    Feat: 간단한 블로그 게시판 만듦
    - map(), splice(), push() 등 배열 메서드 활용
    - state와 props 개념 활용
    - onClick(), onChange() 등 이벤트 함수 활용

--- react-blog-app/src/App.js
+++ react-blog-app/src/App.js
@@ -11,6 +11,7 @@ function App() {
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
   let [title, setTitle] = useState(0);
+  let [입력값, 입력값변경] = useState("");
 
   return (
     <div className="App">
@@ -29,7 +30,8 @@ function App() {
             >
               {글제목[i]}{" "}
               <span
-                onClick={() => {
+                onClick={(e) => {
+                  e.stopPropagation();
                   let copy = [...따봉];
                   copy[i] = copy[i] + 1;
                   따봉변경(copy);
@@ -40,9 +42,38 @@ function App() {
               {따봉[i]}
             </h4>
             <p>2월 17일 발행</p>
+            <button
+              onClick={() => {
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                글제목변경(copy);
+              }}
+            >
+              삭제
+            </button>
           </div>
         );
       })}
+
+      <input
+        onChange={(e) => {
+          입력값변경(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          if (입력값) {
+            let copy = [...글제목];
+            copy.unshift(입력값);
+            글제목변경(copy);
+            따봉.push(0);
+          } else {
+            return;
+          }
+        }}
+      >
+        글수정
+      </button>
 
       {modal == true ? <Modal 글제목={글제목} title={title} /> : null}
     </div>
